b0bb59170a50049c1b9f2a56b2591516a47112ec
Anthony Work PC <anthonyguerrero62@outlook.com>
2023-07-12 23:59:25 -0700

    added date created on user pages

--- src/pages/UserPage/UserPage.js
+++ src/pages/UserPage/UserPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 // import API from "../../utils/API";
+import DayJS from 'react-dayjs';
 import Header from '../../components/Header';
 import Navbar from '../../components/Navbar/navbar';
 import Footer from '../../components/Footer';
@@ -24,7 +25,7 @@ function UserPage(props) {
     <main className='pr-main'>
       <h1 className='profile-username'>{props.title} By: {props.username}</h1>
       <h2 className='profile-fullname'>{props.text}</h2>
-      <h2 className='profile-email'>{props.createdAt}</h2>
+      <h2 className='profile-email'>Created On: <DayJS format="MM-DD-YYYY">{props.createdAt}</DayJS></h2>
       {/* <h2 className='profile-pages'>Pages: {pages.length}</h2> */}
     </main>
         <Footer />
