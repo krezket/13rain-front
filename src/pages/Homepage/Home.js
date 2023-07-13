95139b0a5e143cd37a867d0ee8f8a7c71718d763
Anthony Guerrero <krezket@Anthonys-Mac-Studio.local>
2023-07-13 12:25:54 -0700

    added bio to profile

--- src/pages/Homepage/Home.js
+++ src/pages/Homepage/Home.js
@@ -53,7 +53,7 @@ export default function Home(props) {
                     <img id='welcome' src={welcome} alt='welcome'></img>
                 </header>
                 <article id='hp-article'>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                    <p id='home-intro'>Inspired by the styles of the old net, "13rainstorm" is an experimental webpage designied as a simple social media platform. Here a user can create and login to their account, and create pages. ♥ ♦ ♣ ♠</p>
                 </article>
             </section>
             <aside className='aside-right'>
