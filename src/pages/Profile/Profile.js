888b59760ffd8aae7bbacaa46925f2ff8c38c0d1
Anthony Guerrero <krezket@Anthonys-Mac-Studio.local>
2023-07-13 22:56:48 -0700

    added input and text area to edit profile page

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -65,7 +65,7 @@ return (
     
      : 
      <div>
-            <Header />
+        <Header />
       <nav>
         <Navbar
           type={props.type}
@@ -77,32 +77,27 @@ return (
           setToken={props.setToken}
         />
       </nav>
-      <main className='pr-main'>
-        <div className='h1-ar'>
-        <h1 className='profile-username'>{props.username}</h1>
-        <article className='profile-bio'>
+      <main className='form-main'>
+
+        <form className='edit-form'>
+          <input className='profile-username' placeholder={props.username}></input>
+          <input className='profile-fullname' placeholder={props.fullName}></input>
+          <input className='profile-email' placeholder={props.email}></input>
+
           {props.bio === "" ?
-          <p>no bio yet</p>
-          // <form onSubmit={submitHandler}>
-          //   <textarea placeholder='write your bio' name='bioText' value={bioText} onChange={handleChange}></textarea>
-          //   <button>submit</button>
-          // </form>
-            :
-            <p>{props.bio}</p>
+            <textarea placeholder='write a bio'></textarea>
+              :
+            <textarea>{props.bio}</textarea>
           }
-        </article>
-        </div>
-        {!props.pages ?
-        <h2 className='profile-pages'>Total Pages: 0</h2>
-        :
-        <h2 className='profile-pages'>Total Pages: {props.pages.length}</h2>
-        }
+          <button>Submit</button>
+        </form>
+        
       </main>
       <Footer/>
      </div>
     }
     </div>
-  )
-}
+  );
+};
 
 export default Profile
