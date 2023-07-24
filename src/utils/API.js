d72788854bd4817a897bda574fac1a3bb59eeaab
Anthony Work PC <anthonyguerrero62@outlook.com>
2023-07-23 20:01:40 -0700

    added get by name

--- src/utils/API.js
+++ src/utils/API.js
@@ -41,6 +41,21 @@ const API = {
         }
     },
 
+    // GET BY USERNAME FOR PROFILE / USER
+    getProfileByName: async (x) => {
+        try {
+            const dbProfileData = await fetch(`${URL_PREFIX}/users/profile/${x}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            return dbProfileData.json();
+        } catch (error) {
+            console.log(error);
+        }
+    },
+
     //get all profiles
     getProfiles: async () => {
         try {
