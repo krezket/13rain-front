45050f555be20ba9c702d923d2c57e401edc2764
Anthony Work PC <anthonyguerrero62@outlook.com>
2023-07-31 22:52:39 -0700

    changed title again

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,13 +8,13 @@ export default function Header() {
     return (
         <header className='header'>
             <Link className='home-link' to='/home'>
-                <h1 className='title'>▌│█║▌║▌║ 𐌊𐍂𐌴Ɀ𐌊𐌴𐨠 ║▌║▌║█│▌</h1>
+                {/* <h1 className='title'>▌│█║▌║▌║ 𐌊𐍂𐌴Ɀ𐌊𐌴𐨠 ║▌║▌║█│▌</h1> */}
                 {/* <h1 className='title'>13𝖗𝖆𝖎𝖓𝖘𝖙𝖔𝖗𝖒</h1> */}
                 {/* <h1 className='title'>▌│█║▌║▌║ ƘⱤƸⱿƘƸƬ ║▌║▌║█│▌</h1> */}
                 {/* <h1 className='title'>❂𐂷 ƘⱤƸⱿƘƸƬ 𐂷❂</h1> */}
                 {/* <h1 className='title'>𐌊𐌓𐌄Ɀ𐌊𐌄𐌕</h1> */}
                 {/* <h1 className='title'>▌│█║▌║▌║ KREZKET ║▌║▌║█│▌</h1> */}
-                {/* <h1 className='title'>𝐾𝑅𝐸𝑍𝐾𝐸𝑇</h1> */}
+                <h1 className='title'>𝐾𝑅𝐸𝑍𝐾𝐸𝑇</h1>
             </Link>
         </header>
     );
