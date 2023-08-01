958f781f574eff5a883a24fef2dfd2758789d85f
Anthony Guerrero <krezket@Anthonys-Mac-Studio.local>
2023-08-01 09:19:50 -0700

    added transiton to button

--- src/pages/Enter/theGate.js
+++ src/pages/Enter/theGate.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './style.css'
 import { Link } from 'react-router-dom'
-import enter from '../../assets/enter/enter-8.gif'
+import enter from '../../assets/enter/enter-6.gif'
 import smokeskull from '../../assets/death/smokeskull.gif'
 
 function Enter() {
   return (
-    <div id='enter'>
+    <div className='enter'>
       <img src={smokeskull} alt='smoking skull'></img>
       <Link to='/home'>
-        <img src={enter} alt='enter-here'></img>
+        <img className='btn' src={enter} alt='enter-here'></img>
       </Link>
     </div>
   );
